6fc2dcc24c7eb8c04c18d9954a9056ff553b41da
Ric Allinson <capecodehq@yahoo.co.uk>
2013-08-01 14:02:37 -0700

    Imporved assert message

--- index.php
+++ index.php
@@ -8,6 +8,7 @@ assert_options(ASSERT_ACTIVE, 1);
 assert_options(ASSERT_WARNING, 0);
 assert_options(ASSERT_QUIET_EVAL, 1);
 assert_options(ASSERT_CALLBACK, function ($file, $line, $code) {
+    echo "\n\nFailure at line " . $line . " in " . $file . " ";
     throw new Exception();
 });
 
